3e8ebc56ca5484ae1427d10fe54fa6e91ff3f03f
Max <maksym@mac.local.ua>
2023-04-29 13:46:11 +0200

    fixed certificate

--- video-chat-room-backend/src/chat-room.ts
+++ video-chat-room-backend/src/chat-room.ts
@@ -4,13 +4,23 @@ import { Server } from "socket.io";
 import dotenv from 'dotenv';
 
 import { handleSocketConnection } from "./socketServer/socket"
+import * as fs from "fs";
+import * as https from "https";
+import {ENV, ENVIRONMENT, PORT} from "./models/global";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-
 const app = express();
-const server = createServer(app);
+
+export const PORT = process.env.PORT;
+export const ENV = process.env.NODE_ENV;
+
+const options = ENV === ENVIRONMENT.PRODUCTION ? {
+  key: fs.readFileSync('/etc/ssl/private/private.key'),
+  cert: fs.readFileSync('/etc/ssl/certificate.crt'),
+} : {};
+
+const server = https.createServer(options, app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
